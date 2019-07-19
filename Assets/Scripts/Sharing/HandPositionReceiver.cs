4e760bc48ef50bb5d66e26d2a3618afb76d82eb4
Theerapat Muangpoon <chansk131@gmail.com>
2019-07-19 23:12:22 +0100

    add message Receiver and transform callback for hand and laser pointer

---
+++ Assets/Scripts/Sharing/HandPositionReceiver.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class HandPositionReceiver : MonoBehaviour
+{
+    TransformProcesser processor;
+
+    void Start()
+    {
+        processor = GetComponent<TransformProcesser>();
+        CustomMessages.Instance.MessageHandlers[CustomMessages.TestMessageID.HandTransform] = processor.ProcessTransform;
+    }
+}
