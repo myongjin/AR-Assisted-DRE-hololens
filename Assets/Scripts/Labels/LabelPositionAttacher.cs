27ed96fd09470bc8e2d21259acdbb59f33c8a667
Theerapat Muangpoon <chansk131@gmail.com>
2019-07-25 12:56:20 +0100

    fix label positioner

--- Assets/Scripts/Labels/LabelPositionAttacher.cs
+++ Assets/Scripts/Labels/LabelPositionAttacher.cs
@@ -55,52 +55,52 @@ public class LabelPositionAttacher : MonoBehaviour {
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         normal = cam.transform.position - centre.position;
 
-        Quaternion rotation = Quaternion.FromToRotation(normal, Vector3.back);
-        Matrix4x4 m = Matrix4x4.Rotate(rotation);
-
-        for (int i = 0; i < tooltips.Length; i++)
-        {
-            tooltipPositioners[i].angle = Vector3.SignedAngle(tooltipPositioners[0].positioner.pivotPosition - tooltipPositioners[0].positioner.anchor.position, tooltipPositioners[i].positioner.pivotPosition - tooltipPositioners[i].positioner.anchor.position, normal);
-            if (tooltipPositioners[i].angle < 0)
-            {
-                tooltipPositioners[i].angle += 360;
-            }
-            tooltipPositioners[i].projectedPosition = m.MultiplyPoint3x4(tooltipPositioners[i].positioner.pivotPosition);
-        }
+        CalculateAngles();
 
         Array.Sort(tooltipPositioners, (x, y) => x.angle.CompareTo(y.angle));
+
+        //string text = "";
+        //for (int i = 0; i < tooltipPositioners.Length; i++)
+        //{
+        //    text += tooltipPositioners[i].positioner.name + ": " + tooltipPositioners[i].angle + "\n";
+        //}
+        //Debug.Log(text);
+
+        
         bool occlusionResolved = false;
-        int count = 5;
+        int count = 100;
+
+
+        string text = "";
         while (!occlusionResolved && count > 0)
         {
-            for (int i = 0; i < tooltips.Length; i++)
+            occlusionResolved = true;
+            CalculateAngles();
+            string anothertext = "";
+            for (int i = 0; i < tooltipPositioners.Length; i++)
             {
-                tooltipPositioners[i].angle = Vector3.SignedAngle(tooltipPositioners[0].positioner.pivotPosition - tooltipPositioners[0].positioner.anchor.position, tooltipPositioners[i].positioner.pivotPosition - tooltipPositioners[i].positioner.anchor.position, normal);
-                if (tooltipPositioners[i].angle < 0)
-                {
-                    tooltipPositioners[i].angle += 360;
-                }
-                tooltipPositioners[i].projectedPosition = m.MultiplyPoint3x4(tooltipPositioners[i].positioner.pivotPosition);
+                anothertext += tooltipPositioners[i].positioner.name + ": " + tooltipPositioners[i].angle + "\n";
             }
 
-            string text = "";
+            text = "";
             for (int i = 0; i < tooltipPositioners.Length; i++)
             {
                 bool isOccludded = false;
                 float moveAngle = 0.0f;
                 text += tooltipPositioners[i].positioner.gameObject.name + " " + tooltipPositioners[i].angle + " ";
 
-                if (Math.Abs(tooltipPositioners[i].projectedPosition.x - tooltipPositioners[(i + 1) % tooltipPositioners.Length].projectedPosition.x) < 0.14 && Math.Abs(tooltipPositioners[i].projectedPosition.y - tooltipPositioners[(i + 1) % tooltipPositioners.Length].projectedPosition.y) < 0.05)
+                if (HorizontalDistance(i, NextElem(i)) < 0.14 && VirticalDistance(i, NextElem(i)) < 0.08)
                 {
                     text += "R";
                     isOccludded = true;
                     moveAngle -= offsetAngle;
                 }
 
-                if (Math.Abs(tooltipPositioners[i].projectedPosition.x - tooltipPositioners[(i - 1 + tooltipPositioners.Length) % tooltipPositioners.Length].projectedPosition.x) < 0.14 && Math.Abs(tooltipPositioners[i].projectedPosition.y - tooltipPositioners[(i - 1 + tooltipPositioners.Length) % tooltipPositioners.Length].projectedPosition.y) < 0.05)
+                if (HorizontalDistance(i, PrevElem(i)) < 0.14 && VirticalDistance(i, PrevElem(i)) < 0.05)
                 {
                     text += "L";
                     isOccludded = true;
@@ -121,12 +121,56 @@ public class LabelPositionAttacher : MonoBehaviour {
                     //Matrix4x4 mPivot = Matrix4x4.Rotate(rotation);
                     //tooltipPositioners[i].positioner.pivot.position = mPivot.MultiplyPoint3x4(tooltipPositioners[i].positioner.pivotPosition);
                     tooltipPositioners[i].positioner.pivotPosition = tooltipPositioners[i].positioner.pivotPosition.RotateAroundPivot(centre.position, pivotRotation);
+                    tooltipPositioners[i].positioner.pivot.position = tooltipPositioners[i].positioner.pivotPosition;
+                    //Debug.Log(tooltipPositioners[i].positioner.gameObject.name + " " + moveAngle);
                 }
-                occlusionResolved = !isOccludded;
-                count -= 1;
+                occlusionResolved &= !isOccludded;
 
             }
+            count -= 1;
         }
-        //Debug.Log(text);
+        Debug.Log(text);
+
+    }
+
+    private float HorizontalDistance(int i, int j)
+    {
+        return Math.Abs(tooltipPositioners[i].projectedPosition.x - tooltipPositioners[j].projectedPosition.x);
+    }
+
+    private float VirticalDistance(int i, int j)
+    {
+        return Math.Abs(tooltipPositioners[i].projectedPosition.y - tooltipPositioners[j].projectedPosition.y);
+    }
+
+    private int PrevElem(int i)
+    {
+        return (i - 1 + tooltipPositioners.Length) % tooltipPositioners.Length;
+    }
+
+    private int NextElem(int i)
+    {
+        return (i + 1) % tooltipPositioners.Length;
+    }
+
+    private void CalculateAngles()
+    {
+        Quaternion rotation = Quaternion.FromToRotation(normal, Vector3.back);
+        Matrix4x4 m = Matrix4x4.Rotate(rotation);
+
+        for (int i = 0; i < tooltips.Length; i++)
+        {
+            tooltipPositioners[i].angle = Vector3.SignedAngle(AnchorToPivotVector(0), AnchorToPivotVector(i), normal);
+            if (tooltipPositioners[i].angle < 0)
+            {
+                tooltipPositioners[i].angle += 360;
+            }
+            tooltipPositioners[i].projectedPosition = m.MultiplyPoint3x4(tooltipPositioners[i].positioner.pivotPosition);
+        }
+    }
+
+    private Vector3 AnchorToPivotVector(int elem)
+    {
+        return tooltipPositioners[elem].positioner.pivotPosition - tooltipPositioners[elem].positioner.anchor.position;
     }
 }
