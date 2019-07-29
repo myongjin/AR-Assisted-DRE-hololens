7d222337f7f1c62001215d6e53bd535137b90b61
Theerapat Muangpoon <chansk131@gmail.com>
2019-07-29 10:14:35 +0100

    add force visualisation

--- Assets/Scripts/UI/ForceVisualiser.cs
+++ Assets/Scripts/UI/ForceVisualiser.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ForceVisualiser : MonoBehaviour
 {
     public TextMesh forceText;
+
+    [SerializeField]
+    private Image foregroundImage;
+    [SerializeField]
+    private float forceMax = 10;
+
+    private float force;
 
     // Use this for initialization
     private void Start()
@@ -15,7 +23,9 @@ public class ForceVisualiser : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        forceText.text = ForceProcessor.Instance.Force.ToString("F2");
+        force = ForceProcessor.Instance.Force;
+        forceText.text = force.ToString("F2");
 
+        foregroundImage.fillAmount = force / forceMax;
     }
 }
